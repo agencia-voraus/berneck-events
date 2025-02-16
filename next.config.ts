a40d9cacc01c1764ef590cfd18ee445b4b44e761
Allan Camargo <allan.mdev@gmail.com>
2025-02-16 15:14:06 -0300

    adjust

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  output: 'standalone',
-  trailingSlash: false,
-  headers: async () => [
-    {
-      source: '/(.*)',
-      headers: [
-        {
-          key: 'Cache-Control',
-          value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
-        },
-      ],
-    },
-  ],
-  redirects: async () => [
-    {
-      source: '/admin',
-      destination: '/admin/menu',
-      permanent: false,
-    },
-  ],
+
 };
 
 export default nextConfig;
