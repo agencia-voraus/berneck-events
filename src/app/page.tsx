1fa35d2b5979a90c58ca4241fd6fc2d0e71d5002
Allan Camargo <allan.mdev@gmail.com>
2025-02-17 17:34:10 -0300

    feat: add first screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,53 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import React from "react";
+import { Button } from "@/components/Button";
 import Image from "next/image";
+import { Footer } from "@/components/Footer";
+import { useRouter } from "next/navigation"; // Importe o useRouter
 
 const Menu = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [isFadingOut, setIsFadingOut] = useState(false);
-  const router = useRouter();
+  const router = useRouter(); // Inicialize o useRouter
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsFadingOut(true);
-      setTimeout(() => {
-        setIsLoading(false);
-        router.push("/client");
-      }, 500);
-    }, 2000); 
-
-    return () => clearTimeout(timer);
-  }, [router]);
-
-  if (isLoading) {
-    return (
-      <div className={`flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-blue-50 to-green-50 ${isFadingOut ? 'fade-out' : ''}`}>
-        <div className="animate-spin rounded-full h-24 w-24 border-t-4 border-b-4 border-accent-green transition-opacity duration-500 opacity-100"></div>
-        <Image 
-          src="/logo.png" 
-          alt="Logo"
-          width={160} 
-          height={160} 
-          className="w-32 md:w-40 mt-8 transition-opacity duration-500 opacity-100" 
-        />
-        <p className="mt-4 text-gray-800 text-lg font-semibold transition-opacity duration-500 opacity-100">
-          Carregando seu gift...
-        </p>
-        <p className="mt-2 text-gray-600 text-sm transition-opacity duration-500 opacity-100">
-          Isso pode levar alguns segundos
-        </p>
-      </div>
-    );
-  }
+  const handleCadastrar = () => {
+    router.push("/client"); // Redireciona para a rota /client
+  };
 
   return (
-    <></>
+    <div className="min-h-screen flex flex-col items-center">
+      <div className="w-full max-h-[600px] overflow-hidden flex justify-center items-center ">
+        <Image
+          src="/banner.jpg"
+          alt="Banner Expo Revestir 2025"
+          width={1200}
+          height={800}
+          className="w-full h-full object-contain"
+        />
+      </div>
+
+      <div className="bg-white rounded-2xl  p-6 mt-4 text-center w-80">
+        <Image
+          src="/logo.png"
+          alt="Berneck Logo"
+          width={240}
+          height={100}
+          className="max-w-60 mb-8 mx-auto"
+        />
+        <p className="text-2xl mb-2">Cadastre-se</p>
+        <p className="text-2xl mb-4">
+          e tenha acesso a um <br />
+          <strong className="text-4xl text-border-primary">Gift Exclusivo</strong>
+        </p>
+        <Button
+          onClick={handleCadastrar}
+          className="bg-accent-green text-white w-full py-2 rounded-xl hover:bg-green-800"
+        >
+          Cadastrar-se
+        </Button>
+      </div>
+
+      <Footer />
+    </div>
   );
 };
 
