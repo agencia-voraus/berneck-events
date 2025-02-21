3cb27fe9451c2ea89fcf31c63201968dce487f16
Allan Camargo <allan.mdev@gmail.com>
2025-02-21 02:13:22 -0300

    fix

--- src/app/admin/account/page.tsx
+++ src/app/admin/account/page.tsx
@@ -10,27 +10,27 @@ const Account = () => {
 
   const handleLogout = async () => {
     console.log("Logout iniciado no cliente");
-      try {
-        const response = await fetch("/api/auth/logout", {
-          method: "POST",
-          credentials: "include",
-        });
-    
-        if (response.ok) {
-          localStorage.clear();
-          sessionStorage.clear();
-          document.cookie = "token=; Max-Age=0; path=/;"; 
-    
-          router.replace("/admin");
-          window.location.reload(); 
-        } else {
-          console.error("Erro ao fazer logout");
-        }
-      } catch (error) {
-        console.error("Erro ao tentar sair:", error);
+    try {
+      const response = await fetch("/api/auth/logout", {
+        method: "POST",
+        credentials: "include",
+      });
+  
+      if (response.ok) {
+        localStorage.clear();
+        sessionStorage.clear();
+        document.cookie = "token=; Max-Age=0; path=/;";
+  
+        router.replace("/admin");
+        window.location.reload();
+      } else {
+        console.error("Erro ao fazer logout");
       }
-    };
+    } catch (error) {
+      console.error("Erro ao tentar sair:", error);
+    }
   };
+  
 
   return (
     <>
