c62a1b3dfc92f014fb9e9eb257a33b6bca2d1d69
Allan Camargo <allan.mdev@gmail.com>
2025-02-27 02:04:12 -0300

    fix: update const

--- src/app/api/gift/route.ts
+++ src/app/api/gift/route.ts
@@ -3,43 +3,42 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-
 export async function GET(req: Request) {
-    try {
-      const { searchParams } = new URL(req.url);
-      const leadId = searchParams.get("leadId");
-  
-      if (!leadId) {
-        return NextResponse.json(
-          { message: "Lead ID é obrigatório" },
-          { status: 400 }
-        );
-      }
-  
-      const gift = await prisma.gift.findFirst({
-        where: { leadId },
-        orderBy: { createdAt: 'desc' },
-        select: {
-          isPhysical: true,
-          hasClaimed: true,
-          code: true,
-          createdAt: true,
-        },
-      });
-  
-      if (!gift) {
-        return NextResponse.json(
-          { message: "Gift não encontrado para este Lead" },
-          { status: 404 }
-        );
-      }
-  
-      return NextResponse.json(gift, { status: 200 });
-    } catch (error) {
-      console.error("Erro ao buscar gift:", error);
+  try {
+    const { searchParams } = new URL(req.url);
+    const leadId = searchParams.get("leadId");
+
+    if (!leadId) {
       return NextResponse.json(
-        { message: "Erro interno do servidor" },
-        { status: 500 }
+        { message: "Lead ID é obrigatório" },
+        { status: 400 }
       );
     }
+
+    const gift = await prisma.gift.findFirst({
+      where: { leadId: leadId },
+      orderBy: { createdAt: "desc" },
+      select: {
+        isPhysical: true,
+        hasClaimed: true,
+        code: true,
+        createdAt: true,
+      },
+    });
+
+    if (!gift) {
+      return NextResponse.json(
+        { message: "Gift não encontrado para este Lead" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(gift, { status: 200 });
+  } catch (error) {
+    console.error("Erro ao buscar gift:", error);
+    return NextResponse.json(
+      { message: "Erro interno do servidor" },
+      { status: 500 }
+    );
   }
+}
